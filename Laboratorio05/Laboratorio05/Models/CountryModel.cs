e62a369b7c81770b81a724feb12fb21649cacfb8
LeonelCamposM <leonelscamposm@gmail.com>
2022-05-28 14:15:38 -0600

    Modified model: la06

--- Laboratorio05/Laboratorio05/Models/CountryModel.cs
+++ Laboratorio05/Laboratorio05/Models/CountryModel.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Laboratorio05.Models
 {
     public class CountryModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar un nombre")]
+        [DisplayName("Nombre del país")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar un continente")]
+        [DisplayName("Continente")]
         public string Continent { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar un idioma")]
+        [DisplayName("Idioma")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "No puede ingresar numeros")]
         public string Idiom { get; set; }
     }
 }
